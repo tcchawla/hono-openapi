023245419c7b956c08db55aecb88cb42c6a616ed
tcchawla <tc.chawla2000@gmail.com>
2025-02-22 18:06:26 +0530

    init commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import app from "./app.js";
+import { serve } from "@hono/node-server";
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);
